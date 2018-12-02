404c477dd0eb5c1f7a64b3ca4905c0d193ce28da
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2018-12-02 13:42:30 +0100

    Dodana validacija i za JMBG

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
+import java.time.LocalDate;
 import java.util.function.Function;
 
 public class Controller {
@@ -58,9 +59,54 @@ public class Controller {
         return (unos.length() == 5 && unos.matches("[0-9]+"));
     }
     private boolean validnoImePrezime(String unos) {
-
         return (unos.length() >= 1 && unos.length() <= 20 && unos.matches("[a-zA-Z]+"));
     }
+
+    private boolean validanJMBG(String unos){
+        if (unos.length() != 13 || !unos.matches("[0-9]+"))
+            return false;
+        int[] duzinaMjeseci = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int dan = getJMBGDan();
+        int mjesec = getJMBGMjesec();
+        int godina = getJMBGGodina();
+        if (mjesec <= 0 || mjesec > 12)
+            return false;
+        if (godina % 400 == 0 || (godina % 100 != 0 && godina % 4 == 0))
+            duzinaMjeseci[1] = 29;
+        return  ((dan > 0 && dan <= duzinaMjeseci[mjesec - 1]));
+        
+    }
+
+    private int getJMBGDan() {
+        return Integer.parseInt(getJMBG().substring(0, 2));
+    }
+
+    private String getJMBG() {
+        return JMBGField.getText();
+    }
+
+    private int getJMBGMjesec() {
+        return Integer.parseInt(getJMBG().substring(2, 4));
+    }
+
+    private int getJMBGGodina() {
+        int godina = Integer.parseInt(getJMBG().substring(4, 7));
+        if (godina >= 900)
+            godina += 1000;
+        else
+            godina += 2000;
+        return godina;
+    }
+
+    private boolean validanDatumRodjenja() {
+        if (dateField.getValue() == null)
+            return false;
+        int dan = dateField.getValue().getDayOfMonth();
+        int mjesec = dateField.getValue().getMonthValue();
+        int godina = dateField.getValue().getYear();
+        return ((dateField.getValue().compareTo(LocalDate.now()) <= 0) && validanJMBG(getJMBG()) && dan == getJMBGDan() && mjesec == getJMBGMjesec() && godina == getJMBGGodina());
+    }
+
     private boolean validanEmail(String unos) {
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
@@ -98,6 +144,7 @@ public class Controller {
         provjerUnosa(indeksField, this::validanIndeks);
         provjerUnosa(telefonField, this::validanTelefon);
         provjerUnosa(emailField, this::validanEmail);
+        provjerUnosa(JMBGField, this::validanJMBG);
 
     }
 
